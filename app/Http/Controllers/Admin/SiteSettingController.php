40c325f5ba47cc9539cb2a92f0e08e585f2d9a42
Shubhashish Bhowmik <shuvobhowmik_cse11uits@yahoo.com>
2024-07-06 11:08:59 +0600

    181. General Setting-> workign with update form

--- app/Http/Controllers/Admin/SiteSettingController.php
+++ app/Http/Controllers/Admin/SiteSettingController.php
@@ -3,11 +3,31 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\admin\GeneralSettingUpdateRequest;
+use App\Models\SiteSetting;
+use App\Services\Notify;
 use Illuminate\Http\Request;
 
 class SiteSettingController extends Controller
 {
     public function Index(){
         return view('admin.site-setting.index');
+    }
+
+    function updateGeneralSetting(GeneralSettingUpdateRequest $request){
+        $validatedData = $request->validated();
+
+        foreach($validatedData as $key => $value) {
+            SiteSetting::updateOrCreate(
+                ['key' => $key],
+                ['value' => $value]
+            );
+
+        }
+
+        Notify::updateNotification();
+
+        return redirect()->back();
+
     }
 }
---
+++ app/Http/Requests/admin/GeneralSettingUpdateRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class GeneralSettingUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'site_name' => ['required', 'max:255'],
+            'site_email' => ['required', 'max:255', 'email'],
+            'site_phone' => ['required', 'max:255'],
+            'site_map' => ['nullable'],
+            'site_default_currency' => ['required'],
+            'site_currency_icon' => ['required'],
+
+        ];
+    }
+}
