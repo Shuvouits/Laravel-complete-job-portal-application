b0c2894da64d286952adf171da2e57117591f57b
Shubhashish Bhowmik <shuvobhowmik_cse11uits@yahoo.com>
2024-08-19 11:15:46 +0600

    208.6 showing order

--- app/Http/Controllers/frontend/CompanyOrderController.php
+++ app/Http/Controllers/frontend/CompanyOrderController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\frontend;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
 use Illuminate\Http\Request;
+use LaravelDaily\Invoices\Invoice;
+use LaravelDaily\Invoices\Classes\Buyer;
+use LaravelDaily\Invoices\Classes\InvoiceItem;
+use LaravelDaily\Invoices\Classes\Party;
 
 class CompanyOrderController extends Controller
 {
@@ -12,4 +16,45 @@ class CompanyOrderController extends Controller
         $orders = Order::where('company_id', auth()->user()->company->id)->paginate(20);
         return view('frontend.company-dashboard.order.index', compact('orders'));
     }
+
+    function show(string $id){
+        $order = Order::findOrFail($id);
+        return view('frontend.company-dashboard.order.show', compact('order'));
+    }
+
+    function invoice(string $id) {
+        $order = Order::findOrFail($id);
+
+        $customer = new Buyer([
+            'name'          => $order->company->name,
+            'custom_fields' => [
+                'email' => $order->company->email,
+                'transaction' => $order->transaction_id,
+                'payment method' => $order->payment_provider,
+            ],
+        ]);
+
+        $seller = new Party([
+            'name'          => config('settings.site_name'),
+            'phone'         => config('settings.site_phone'),
+            'custom_fields' => [
+                'email' => config('settings.site_email')
+            ],
+        ]);
+
+        $item = InvoiceItem::make($order->package_name.' Plan')->pricePerUnit($order->amount);
+
+        $invoice = Invoice::make()
+            ->series($order->order_id)
+            ->currencyCode($order->paid_in_currency)
+            ->currencySymbol($order->paid_in_currency)
+            ->buyer($customer)
+            ->seller($seller)
+            ->status('paid')
+            ->payUntilDays(0)
+            ->addItem($item);
+
+        return $invoice->download();
+    }
+
 }
--- resources/views/frontend/company-dashboard/order/index.blade.php
+++ resources/views/frontend/company-dashboard/order/index.blade.php
@@ -31,67 +31,46 @@
                         <h3 class="mt-0 mb-15 color-brand-1 text-center">All Orders</h3>
 
                         <div>
-                            <table class="table table-bordered table-hover">
-                                <thead>
-                                    <tr>
-                                        <th>Order & Transaction</th>
-                                        <th>Company</th>
-                                        <th>Plan</th>
-                                        <th>Paid Amount</th>
-                                        <th>Main Price</th>
-                                        <th>Payment Method</th>
-                                        <th>Payment Status</th>
-                                        <th style="width: 10%">Action</th>
-                                    </tr>
-                                </thead>
+                            <table class="table table-hover">
+                                <tr>
+                                    <th>Order No</th>
+
+                                    <th>Plan</th>
+                                    <th>Paid Amount</th>
+
+                                    <th>Payment Method</th>
+                                    <th>Payment Status</th>
+                                    <th style="width: 10%">Action</th>
+                                </tr>
                                 <tbody>
                                     @foreach ($orders as $order)
-                                    <tr>
-                                        <td>
-                                            #{{ $order->order_id }}
-                                            <br>
-                                            Transaction: {{ $order->transaction_id }}
-                                        </td>
+                                        <tr>
+                                            <td>
+                                                #{{ $order->order_id }}
+                                            </td>
 
-                                        <td>
-                                            <div class="d-flex">
-                                                <div class="mr-2">
-                                                    <img style="width: 50px; height:50px; object-fit:cover"
-                                                        src="{{ asset($order->company->logo) }}" alt="">
-                                                </div>
-                                                <div>
-                                                    <b>{{ $order->company->name }}</b>
-                                                    <br>
-                                                    {{ $order->company->email }}
+                                            <td>
+                                                {{ $order->package_name }}
+                                            </td>
 
-                                                </div>
-                                            </div>
-                                        </td>
+                                            <td>
+                                                {{ $order->amount }} {{ $order->paid_in_currency }}
+                                            </td>
 
-                                        <td>
-                                            {{ $order->package_name }}
-                                        </td>
+                                            <td>
+                                                {{ $order->payment_provider }}
+                                            </td>
+                                            <td>
+                                                <p class="badge bg-primary text-light">{{ $order->payment_status }}</p>
+                                            </td>
 
-                                        <td>
-                                            {{ $order->amount }} {{ $order->paid_in_currency }}
-                                        </td>
-                                        <td>
-                                            {{ $order->default_amount }}
-                                        </td>
-                                        <td>
-                                            {{ $order->payment_provider }}
-                                        </td>
-                                        <td>
-                                            <p class="badge bg-primary text-light">{{ $order->payment_status }}</p>
-                                        </td>
+                                            <td>
+                                                <a href="{{ route('orders.show', $order->id) }}"
+                                                    class="btn-sm btn btn-primary"><i class="fas fa-eye"></i></a>
 
-                                        <td>
-                                            <a href="{{ route('admin.orders.show', $order->id) }}"
-                                                class="btn-sm btn btn-primary">View Details</a>
-
-                                        </td>
-                                    </tr>
-                                @endforeach
+                                            </td>
+                                        </tr>
+                                    @endforeach
 
                                 </tbody>
 
