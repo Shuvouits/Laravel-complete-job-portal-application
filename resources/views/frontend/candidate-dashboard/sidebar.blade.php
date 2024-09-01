1349906e7d63a604b43be8d852f34914532ae2fb
Shuvouits <shuvobhowmik_cse11uits@yahoo.com>
2024-09-01 22:51:15 +0600

    candidate sidebar active

--- resources/views/frontend/candidate-dashboard/sidebar.blade.php
+++ resources/views/frontend/candidate-dashboard/sidebar.blade.php
@@ -1,12 +1,12 @@
 <div class="col-lg-3 col-md-4 col-sm-12">
     <div class="box-nav-tabs nav-tavs-profile mb-5">
         <ul class="nav" role="tablist">
-            <li><a class="btn btn-border mb-20 active" href="{{ route('candidate.dashboard') }}">Dashboard</a>
+            <li><a class="btn btn-border mb-20 {{ setSidebarActive(['candidate.dashboard']) }} " href="{{ route('candidate.dashboard') }}">Dashboard</a>
             </li>
-            <li><a class="btn btn-border mb-20" href="{{ route('candidate.applied-jobs.index') }}">Applied Jobs</a></li>
-            <li><a class="btn btn-border mb-20" href="{{ route('candidate.bookmarked-jobs.index') }}">Bookmarked</a>
+            <li><a class="btn btn-border mb-20 {{ setSidebarActive(['candidate.applied-jobs.index']) }} " href="{{ route('candidate.applied-jobs.index') }}">Applied Jobs</a></li>
+            <li><a class="btn btn-border mb-20  {{ setSidebarActive(['candidate.bookmarked-jobs.index']) }}" href="{{ route('candidate.bookmarked-jobs.index') }}">Bookmarked</a>
             </li>
-            <li><a class="btn btn-border mb-20" href="{{ route('candidate.profile.index') }}">My Profile</a></li>
+            <li><a class="btn btn-border mb-20 {{ setSidebarActive(['candidate.profile.index']) }} " href="{{ route('candidate.profile.index') }}">My Profile</a></li>
             <li>
                  <!-- Authentication -->
                  <form method="POST" action="{{ route('logout') }}">
