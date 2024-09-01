35f4fe61cebb0e30102295f7b6035f18c4c62fe5
Shuvouits <shuvobhowmik_cse11uits@yahoo.com>
2024-09-01 22:13:28 +0600

    287. 4_Applied Job - Showing dynamic Data in Dashboard (Part - 1)

--- app/Http/Controllers/frontend/CandidateMyJobController.php
+++ app/Http/Controllers/frontend/CandidateMyJobController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\AppliedJob;
 use Illuminate\Http\Request;
 
 class CandidateMyJobController extends Controller
 {
-    //
+    function index() {
+        $appliedJobs = AppliedJob::with('job')->where('candidate_id', auth()->user()->id)->paginate();
+        return view('frontend.candidate-dashboard.my-job.index', compact('appliedJobs'));
+    }
 }
